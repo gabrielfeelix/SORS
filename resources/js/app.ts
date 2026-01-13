79a61d0ea60368417be4e61f6b09077394e1d133
Gabriel Felix <gabrielfelix@example.com>
2026-01-13 13:14:54 -0300

    fix: use app name only for document title

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -9,7 +9,7 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 const appName = import.meta.env.VITE_APP_NAME || 'KITAMO';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: () => `${appName}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
